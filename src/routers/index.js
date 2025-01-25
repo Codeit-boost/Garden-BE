e04e08971fef474bd35c53d631826fadbc8e210d
m2nsp <mini071663@gmail.com>
2025-01-25 14:54:17 +0900

    feat: 미션, 꽃 api 추가

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const testRouters = require("./testRouters");
+const flowerRouters = require("./flowerRouters");
+const missionRouters = require("./missionRouters");
 
 /**
  * @swagger
@@ -9,5 +11,21 @@ const testRouters = require("./testRouters");
  *   description: 테스트 API 관련 엔드포인트
  */
 router.use("", testRouters);
+
+/**
+ * @swagger
+ * tags:
+ *   name: Flowers
+ *   description: 꽃 관련 API
+ */
+router.use("/flower", flowerRouters);
+
+/**
+ * @swagger
+ * tags:
+ *   name: Missions
+ *   description: 미션 관련 API
+ */
+router.use("/mission", missionRouters);
 
 module.exports = router;
