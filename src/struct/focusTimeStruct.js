739a0fdd829c3bd99f7cc02df5dc95af812bc5ba
yeobi <kimhyjjang@gmail.com>
2025-01-31 16:11:34 +0900

    feat: 4분의 1 시점마다 DB 업데이트 기능 추가

--- src/struct/focusTimeStruct.js
+++ src/struct/focusTimeStruct.js
@@ -4,7 +4,7 @@ const s = require("superstruct")
  * 집중시간 생성에 사용할 구조체
  */
 const CreateFocusTime = s.object({
-    target_time: s.integer(),
+    target_time: s.string(),
     flower_id: s.integer(),
     category: s.string(),
     member_id: s.integer()
